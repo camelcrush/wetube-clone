1af429c953e258d29c10e37d9dc1705a22dae597
camelcrush <dkxm0927@naver.com>
2022-02-04 23:00:06 +0900

    Middleware: morgan ... logger

--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev"); // morgan()은 함수를 리턴함
 
-const logger = (req, res, next) => {
-  // MiddleWare는  next Argument 가지고 있음
-  console.log(`${req.method} ${req.url}`);
-  next();
+const Home = (req, res) => {
+  return res.send("hello");
 };
-
-const handleHome = (req, res) => {
-  return res.end();
+const login = (req, res) => {
+  return res.send("login");
 };
-
-// app.use(logger) 는 전역(global) Middleware로 사용 가능
-app.get("/", logger, handleHome); // logger Middleware 다음 handleHome controller가 실행됨, 컨트롤러도 next 인수를 가지고 있음.
+app.use(logger);
+app.get("/", Home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
