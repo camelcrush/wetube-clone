bb2c7d1c7d3d2945027751d0b0e4d1c2e09b68c3
camelcrush <dkxm0927@naver.com>
2022-02-04 18:46:46 +0900

    GET Request and Response

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const PORT = 4000;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.end();
+});
+app.get("/login", (req, res) => {
+  return res.send("login here");
+});
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
