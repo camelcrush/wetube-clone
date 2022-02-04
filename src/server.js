86610670a126a334959a2b2add7e8ec61b474f98
camelcrush <dkxm0927@naver.com>
2022-02-04 22:02:29 +0900

    Middleware: next argument, app.use()

--- src/server.js
+++ src/server.js
@@ -4,12 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
+const logger = (req, res, next) => {
+  // MiddleWare는  next Argument 가지고 있음
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const handleHome = (req, res) => {
   return res.end();
-});
-app.get("/login", (req, res) => {
-  return res.send("login here");
-});
+};
+
+// app.use(logger) 는 전역(global) Middleware로 사용 가능
+app.get("/", logger, handleHome); // logger Middleware 다음 handleHome controller가 실행됨, 컨트롤러도 next 인수를 가지고 있음.
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
