3390f68eaa65c65bc95c7cf427373cecf3c9921b
camelcrush <dkxm0927@naver.com>
2022-02-05 16:41:49 +0900

    Clean Code 2: controller

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { trending } from "../controllers/videoController";
+import { join } from "../controllers/userController";
 
 const globalRouter = express.Router(); // 라우터 만들기
 
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome); // 해당 라우터의 컨트롤러 사용하기
+globalRouter.get("/", trending); // 해당 라우터의 컨트롤러 사용하기
+globalRouter.get("/join", join);
 
 export default globalRouter;
