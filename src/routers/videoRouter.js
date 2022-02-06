d9eba5e2f8201e59fa02a2a3b0b3f7fd18fb42d1
camelcrush <dkxm0927@naver.com>
2022-02-06 13:39:44 +0900

    Set pug: app.set(), res.render()

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,7 +4,7 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", see); // url params에 대한 정규표현식(regular expression) : (\\d+)는 숫자만 허용하겠다는 뜻
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
--- src/server.js
+++ src/server.js
@@ -8,8 +8,10 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev"); // morgan()은 함수를 리턴함
-app.use(logger);
 
+app.set("view engine", "pug"); // view engine으로 pug 설정하기
+app.set("views", process.cwd() + "/src/views"); // views 디폴트값을 src폴더 안의 views폴더로 설정하기, cwd()는 current working directory
+app.use(logger);
 app.use("/", globalRouter); // 라우터 사용하기
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
