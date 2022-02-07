6b61c05052b3b8e1e6de357fca33510821e76d2a
camelcrush <dkxm0927@naver.com>
2022-02-07 21:36:13 +0900

    Practice: upload video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { watch, getEdit, postEdit } from "../controllers/videoController";
+import {
+  watch,
+  getEdit,
+  postEdit,
+  getUpload,
+  postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch); // url params에 대한 정규표현식(regular expression) : (\\d+)는 숫자만 허용하겠다는 뜻
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit); // route()를 써서 get(), post()를 한 줄로 표현
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
