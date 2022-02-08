37f12a979e423cd0772494cdc8294d3cbaaed45d
camelcrush <dkxm0927@naver.com>
2022-02-08 23:02:18 +0900

    Video model: mongoose.Schema, model

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import "./db"; // 반드시 파일 자체를 import 해줘야함, server가 파일을 일고 db를 연결함.
+import "./models/Video"; // 모델을 preload시킴, 반드시 db를 import한 후에..
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
