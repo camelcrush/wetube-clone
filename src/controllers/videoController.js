53b41562c49ade0596259d5efea9e9efe0ac31fc
camelcrush <dkxm0927@naver.com>
2022-02-08 23:32:08 +0900

    Query: use callback / model.find({},callback)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,13 @@
 import Video from "../models/Video"; // 모델 import
 
 export const home = (req, res) => {
-  return res.render("home", { pageTitle: "Home" });
+  console.log("Start");
+  Video.find({}, (error, videos) => {
+    // model.find({}, callback): {}은 찾기조건인데 여기서는 모든 데이터를 찾고 함수를 실행한다는 것을 의미
+    console.log("Finished");
+    return res.render("home", { pageTitle: "Home", videos }); // callback에서 return을 함으로써 먼저 db로부터 데이터를 불러오고 난 후에 렌더링을 함
+  });
+  console.log("I finish first");
 };
 export const watch = (req, res) => {
   const { id } = req.params;
