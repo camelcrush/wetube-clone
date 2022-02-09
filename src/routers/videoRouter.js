7cb27128e7e137c84754ae625d2610e9928c439a
camelcrush <dkxm0927@naver.com>
2022-02-09 23:51:14 +0900

    Watch: id params by regular expression, model.findById()

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -9,8 +9,8 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch); // url params에 대한 정규표현식(regular expression) : (\\d+)는 숫자만 허용하겠다는 뜻
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit); // route()를 써서 get(), post()를 한 줄로 표현
+videoRouter.get("/:id([0-9a-f]{24})", watch); // url params에 대한 정규표현식(regular expression) : ([0-9a-f]{24})
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit); // route()를 써서 get(), post()를 한 줄로 표현
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
