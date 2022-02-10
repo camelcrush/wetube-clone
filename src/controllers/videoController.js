29b7842b4cc436804c0e0f1780c929b979702906
camelcrush <dkxm0927@naver.com>
2022-02-10 23:03:37 +0900

    Schema.static(name, function)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,5 @@
 import Video from "../models/Video"; // 모델 import
 
-/*
-console.log("start")
-Video.find({}, (error, videos) => {
-  return res.render("home", { pageTitle: "Home", videos });
-});
-console.log("finished")
-*/
-
 export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
@@ -38,7 +30,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags,
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -53,7 +45,7 @@ export const postUpload = async (req, res) => {
     Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
