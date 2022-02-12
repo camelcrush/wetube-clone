9dbe987f2db08ff51a1a262bb679b21147ffce59
camelcrush <dkxm0927@naver.com>
2022-02-12 15:15:59 +0900

    Login part 1

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { home, search } from "../controllers/videoController";
-import { getJoin, login, postJoin } from "../controllers/userController";
+import {
+  getJoin,
+  getLogin,
+  postJoin,
+  postLogin,
+} from "../controllers/userController";
 
 const rootRouter = express.Router(); // 라우터 만들기
 
 rootRouter.get("/", home); // 해당 라우터의 컨트롤러 사용하기
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
