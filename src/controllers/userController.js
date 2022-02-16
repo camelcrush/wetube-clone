435c56df7263b4eeb65d2fd0e66306fee3f6f6e3
camelcrush <dkxm0927@naver.com>
2022-02-16 20:48:43 +0900

    populate() for relation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
+import Video from "../models/Video";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -203,5 +204,6 @@ export const see = async (req, res) => {
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
-  return res.render("users/profile", { pageTitle: "Profile", user });
+  const videos = await Video.find({ owner: user._id });
+  return res.render("users/profile", { pageTitle: "Profile", user, videos });
 };
