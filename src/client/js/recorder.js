9eeac2397f574d95bd5259fa37d6cc946124a02c
camelcrush <dkxm0927@naver.com>
2022-02-21 22:04:10 +0900

    Video preview: video.srcObject

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+    audio: false,
     video: true,
   });
-  console.log(stream);
+  video.srcObject = stream; // src는 url, srcObj는 객체 또는 다른 형식 데이터를 취함.
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
