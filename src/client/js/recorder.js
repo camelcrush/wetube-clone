121b192c47e976568725d4d9d241051d810adf46
camelcrush <dkxm0927@naver.com>
2022-02-21 22:14:57 +0900

    Recording: MediaRecorder(stream), start(), stop(), ondataavailable

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,41 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream); // stream을 받아 recording
+  recorder.ondataavailable = (e) => {
+    // stop이 실행되면 발동
+    console.log(e);
+    console.log(e.data);
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+    // stop은 ondataavailable 이벤트를 발생 시키고 blop(video file)을 반환함
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
   });
   video.srcObject = stream; // src는 url, srcObj는 객체 또는 다른 형식 데이터를 취함.
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
